2d2dc912d0cbae8440550a284b489fa1be00d5e6
raizrazer <raizrazer@gmail.com>
2023-12-29 10:06:46 +0530

    Serving a file with __dir + 'path'.

--- myApp.js
+++ myApp.js
@@ -1,9 +1,13 @@
 let express = require("express");
 let app = express();
 
-console.log("Hello World");
+// console.log("Hello World");
+// app.get("/", (req, res) => {
+//   res.send("Hello Express");
+// });
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  const path = __dirname + "/views/index.html";
+  res.sendFile(path);
 });
 
 module.exports = app;
