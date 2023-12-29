0aa8086bf8b9b4e1b17b1e513773de4cc1f5b685
raizrazer <raizrazer@gmail.com>
2023-12-29 20:16:20 +0530

    Added middleware lil.

--- myApp.js
+++ myApp.js
@@ -9,6 +9,11 @@ let app = express();
 
 app.use("/public", express.static(__dirname + "/public"));
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
 app.get("/", (req, res) => {
   const path = __dirname + "/views/index.html";
   res.sendFile(path);
