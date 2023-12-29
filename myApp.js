f946dbe11a87e5bbb5fb51677b74046f6a0bce71
raizrazer <raizrazer@gmail.com>
2023-12-29 12:08:05 +0530

    Added /public express.static using middleware.

--- myApp.js
+++ myApp.js
@@ -5,6 +5,9 @@ let app = express();
 // app.get("/", (req, res) => {
 //   res.send("Hello Express");
 // });
+
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
   const path = __dirname + "/views/index.html";
   res.sendFile(path);
